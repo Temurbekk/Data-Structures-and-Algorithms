f265de229336e8519d92d9db25febfbb89bf1e69
Temurbek Sabirov <prodota150@gmail.com>
2019-12-29 17:34:22 -0500

    Implement Min and Max Heap && implement Priority Queue using Max Heap

---
+++ Heaps/MinBinaryHeap.js
@@ -0,0 +1,74 @@
+class MinBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+  insert(val) {
+    this.values.push(val);
+    this.bubbleUp();
+  }
+  //It will make sure the element inserted finds its correct spot in the heap by bubbling up
+  bubbleUp() {
+    let index = this.values.length - 1;
+    let element = this.values[index];
+    while (index < 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+      if (element >= parent) break;
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
+  extractMin() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    //Makes sure you dont replace max with itself when you have one element left in your heap
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.bubbleDown();
+    }
+    return min;
+  }
+  bubbleDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild < element) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild < element) ||
+          (swap !== null && rightChild < leftChild)
+        ) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
+}
+
+let heap = MinBinaryHeap();
+heap.insert(25);
+heap.insert(23);
+heap.insert(12);
+heap.insert(5);
+heap.insert(2);
+heap.insert(42);
+heap.insert(123);
