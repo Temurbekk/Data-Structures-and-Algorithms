d60dede9ca11f880e57fa4a248c8180f2db55322
Temurbek Sabirov <prodota150@gmail.com>
2019-12-30 16:24:30 -0500

    Implement Priority Queue using Binary Heap for faster code

--- Graphs/Dijkstra's_Algorithm_V1.js
+++ Graphs/Dijkstra's_Algorithm_V1.js
@@ -1,19 +1,78 @@
-//Naive Priority Queue without Binary Heap
+//Priority Queue using binary heap
 class PriorityQueue {
   constructor() {
     this.values = [];
   }
-  enqueue(value, priority) {
-    this.values.push({ value, priority });
-    this.sort();
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+  //It will make sure the element inserted finds its correct spot in the heap by bubbling up
+  bubbleUp() {
+    let index = this.values.length - 1;
+    let element = this.values[index];
+    while (index < 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+      if (element.priority <= parent.parent) break;
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
   }
   dequeue() {
-    return this.values.shift();
+    const max = this.values[0];
+    const end = this.values.pop();
+    //Makes sure you dont replace max with itself when you have one element left in your heap
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.bubbleDown();
+    }
+    return max;
   }
-  sort() {
-    this.values.sort((a, b) => a.priority - b.priority);
+  bubbleDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.priority > element.priority) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild.priority > element.priority) ||
+          (swap !== null && rightChild.priority > leftChild.priority)
+        ) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
   }
 }
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
 //Weigthted Graph to hold values on its edges
 class WeightedGraph {
   constructor() {
