ff7f848780ee9b4adcacb66f7e2ae8d4f267fe4e
Temurbek Sabirov <prodota150@gmail.com>
2019-12-30 19:12:11 -0500

    Implement Linear Search

---
+++ Algorithms/Searching/LinearSearch/LinearSearch.js
@@ -0,0 +1,10 @@
+let linearSearch = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+linearSearch([2, 3, 4, 1, 2, 3, 5, 6, 7], 5);
